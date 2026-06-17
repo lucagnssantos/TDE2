f718e6eeb1f61eef2db4f0cf76120f883f0021b6
lucagnssantos <lucas.g.nunes.santos1@gmail.com>
2026-06-17 01:09:16 -0300

    deadlock

---
+++ deadlock/deadlock_reproducao.py
@@ -0,0 +1,31 @@
+import threading
+import time
+
+recurso_a = threading.Lock()
+recurso_b = threading.Lock()
+
+def processo_um():
+    print("T1 tentando adquirir A")
+    with recurso_a:
+        print("T1 adquiriu A")
+        time.sleep(0.05)
+        print("T1 tentando adquirir B")
+        with recurso_b:
+            print("T1 concluiu")
+
+def processo_dois():
+    print("T2 adquiriu B")
+    time.sleep(0.05)
+    print("T2 tentando adquirir A")
+    with recurso_a:
+        print("T2 tentando adiquir A")
+
+thread_um = threading.Thread(target=processo_um)
+thread_dois = threading.Thread(target=processo_dois)
+
+thread_um.start()
+thread_dois.start()
+
+thread_um.join()
+thread_dois.join()
+print("programa finalizado")
